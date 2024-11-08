f5d034e69c626e333cb7d970b225a6ed39d4e3c1
Vladimir Tinica <vltinica@gmail.com>
2024-11-08 05:18:45 -0500

    added menu

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contacts = () => {
+  return (
+   <div><h1>Contacts</h1></div>
+  )
+}
+
+export default Contacts
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Menu from "./Menu";
+
+const Home = () => {
+  return (
+    <div>
+      <Menu/>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
