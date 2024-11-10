dfcfb447c438df7a439a0f62d01e476a8fbc5323
Vladimir Tinica <vltinica@gmail.com>
2024-11-10 16:41:29 -0500

    *

--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const SingleCourse = ({title, slug, id}) => {
+const SingleCourse = ({ title, slug, id }) => {
   return (
     <div>
-        <h2>{title}</h2>
-        <p>Slug: {slug}</p>
-        <p>ID: {id}</p>
+      <h2>{title}</h2>
+      <p>Slug: {slug}</p>
+      <p>ID: {id}</p>
     </div>
-  )
-}
+  );
+};
 
-export default SingleCourse
+export default SingleCourse;
